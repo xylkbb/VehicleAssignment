8c488f9fe4257e7486056c3f955ae00e631c1741
Eric Coe <coeeri001@warren.k12.in.us>
2024-09-12 13:52:19 -0400

    Commit bit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        Vehicle myCar = new Vehicle("Ford", "Fusion");
+        System.out.println(myCar.getMake());
+        System.out.println(myCar.getModel());
+    }
 }
